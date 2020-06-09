1877cc4fe4e3d500884cc7f8be4b914eda60f44b
Ian Chona <ianchonaramirez@gmail.com>
2020-06-08 21:31:22 -0500

    Initialized client and server side of the application

---
+++ backend/server.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+
+require("dotenv/config");
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Middleware
+app.use(cors());
+app.use(express.urlencoded({
+  extended: false
+}));
+app.use(express.json());
+
+// DB Connection
+mongoose
+  .connect(
+    process.env.MONGODB_URI, {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useUnifiedTopology: true,
+    }
+  )
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.log(err));
+
+// Server Init
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
