49bf8f8c3a3b94c9e659b515985f13d048281f1d
Ian Chona <ianchonaramirez@gmail.com>
2020-08-01 19:23:27 -0500

    ...

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ app.use("/products/", productsRoute);
 // Serve static assets if in production
 if (process.env.NODE_ENV === "production") {
 	// Set static folder
-	app.use(express.static("./frontend/build"));
+	//app.use(express.static("./frontend/build"));
 	/*app.use(express.static("D:/home/site/wwwroot/frontend/build"));
 	app.get("*", (req, res) => {
 		res.sendFile(
@@ -41,7 +41,8 @@ if (process.env.NODE_ENV === "production") {
 	});*/
 
 	app.get("*", (req, res) => {
-		res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+		res.sendFile("./server-test.html");
+		//res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
 	});
 }
 
