ee8427fea2ee4612a019d9b0772d9cadaf58b790
Ian Chona <ianchonaramirez@gmail.com>
2020-08-01 19:19:29 -0500

    ...

---
+++ frontend/build/precache-manifest.d7b7bac955cd6084488ed0211336f614.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4a628fd7c72f7e569eb04931dcebb57",
+    "url": "/index.html"
+  },
+  {
+    "revision": "96cf3351cbbf15419615",
+    "url": "/static/css/main.fc17729d.chunk.css"
+  },
+  {
+    "revision": "6871173583172bbae32b",
+    "url": "/static/js/2.d80dd77e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.d80dd77e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "96cf3351cbbf15419615",
+    "url": "/static/js/main.5a7ac746.chunk.js"
+  },
+  {
+    "revision": "b6d1d169a615edca0f4d",
+    "url": "/static/js/runtime-main.b2748aa3.js"
+  }
+]);
--- server.js
+++ server.js
@@ -32,17 +32,17 @@ app.use("/products/", productsRoute);
 // Serve static assets if in production
 if (process.env.NODE_ENV === "production") {
 	// Set static folder
-	//app.use(express.static("./frontend/build"));
-	app.use(express.static("D:/home/site/wwwroot/frontend/build"));
+	app.use(express.static("./frontend/build"));
+	/*app.use(express.static("D:/home/site/wwwroot/frontend/build"));
 	app.get("*", (req, res) => {
 		res.sendFile(
 			path.resolve("D:/home/site/wwwroot/frontend/build/index.html")
 		);
-	});
-
-	/*app.get("*", (req, res) => {
-		res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
 	});*/
+
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+	});
 }
 
 //console.log("Testing building");
