6951e9a767d6de7b98aab92dce7055ecf66924d6
Ian Chona <ianchonaramirez@gmail.com>
2020-08-01 21:33:16 -0500

    ...

---
+++ frontend/build/precache-manifest.f1c3fa257e9e4bfd21886d3e97c62611.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1afcd05ec0a85c1cfd1f69104a841fa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7141363bef04b25af5ba",
+    "url": "/static/css/main.cf5c681b.chunk.css"
+  },
+  {
+    "revision": "6871173583172bbae32b",
+    "url": "/static/js/2.d80dd77e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.d80dd77e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7141363bef04b25af5ba",
+    "url": "/static/js/main.a88a2af7.chunk.js"
+  },
+  {
+    "revision": "b6d1d169a615edca0f4d",
+    "url": "/static/js/runtime-main.b2748aa3.js"
+  }
+]);
